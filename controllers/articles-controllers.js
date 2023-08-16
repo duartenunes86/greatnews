273d144ac28f0bd93b581daf48b3afe622324547
Duarte Nunes <duartenunes4ever@gmail.com>
2023-08-16 08:43:45 +0100

    3 tasks in one

--- controllers/articles-controllers.js
+++ controllers/articles-controllers.js
@@ -1,17 +1,48 @@
 const {
     selectArticleById,
+    selectArticles,
+    selectCommentsByArticle
     
   } = require('../models/articles-models.js');
   
   exports.getArticleById = (req, res, next) => {
-    console.log("fjdijsoijdifjdoijdi")
-    const id = req.params.article_id
-    console.log(id)
-    selectArticleById(id).then((article) => {
+    
+    const {article_id} = req.params
+    
+    
+    selectArticleById(article_id).then((article) => {
+      console.log(article, "Article")
       
+      if (article.length===0 && !isNaN(article_id)) {res.status(404).send({ msg:'article doesn\'t exist' });
+}
+
       res.status(200).send( {article });
-      
-    }) .catch((err) => {
+
+   } ) .catch((err) => {
       next(err);
-    });
-    }
+    });}
+    
+    exports.getArticles = (req, res, next) => {
+        
+        selectArticles().then((results) => {
+
+          
+          res.status(200).send( {results });
+          
+        }) .catch((err) => {
+          next(err);
+        });
+        }
+        exports.getCommentsByArticle = (req, res, next) => {
+        const id =req.params.article_id;
+        console.log(id)
+        
+          selectCommentsByArticle(id).then((results) => {
+            if (results.length===0) {res.status(404).send({ msg:'article doesn\'t exist' });
+      }
+            res.status(200).send( {results });
+            
+          }) .catch((err) => {
+            next(err);
+          });
+          }
--- models/articles-models.js
+++ models/articles-models.js
@@ -5,3 +5,30 @@ exports.selectArticleById = (id) => {
     return result.rows;
   });
 };
+exports.selectArticles = () => {
+    
+    return db.query(`SELECT articles.author,
+    articles.title,
+    articles.article_id,
+    articles.topic,
+    articles.created_at,
+    articles.votes,
+    articles.article_img_url,
+    COUNT(comments.article_id) AS comment_count
+     FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY created_at DESC;`).then((result) => {
+        
+      return result.rows;
+    });
+  };
+
+  exports.selectCommentsByArticle = (id) =>{
+    return db.query(` SELECT   comment_id,
+    votes,
+    created_at,
+    author,
+    body,
+    article_id FROM comments WHERE article_id=${id};`).then((result)=>{
+      return result.rows;
+    })
+
+  }
